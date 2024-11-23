82e183925d8081368e3f487a26e3e909822ddcf5
Alexander Martin <Alexander@theAlexanderMartin.com>
2024-11-23 18:37:06 -0500

    Updated intcode vm structure

--- 2019/Day02/main.py
+++ 2019/Day02/main.py
@@ -7,22 +7,24 @@ def problem(input_file, part2=False):
     if not part2:
         vm = IntcodeVM.from_file(input_file)
         vm.dbg = dbg.debug_level
-#        print(vm)
-        vm.memory[1] = 12
-        vm.memory[2] = 2
+        vm.memory.set(1, 12)
+        vm.memory.set(2, 2)
+        if vm.dbg:
+            print(vm)
         vm.run()
-        return vm.memory[0]
+        return vm.memory.get(0)
 
     target = 19690720
     for noun in range(100):
         for verb in range(100):
             vm = IntcodeVM.from_file(input_file)
             print(f"Trying {noun = }; {verb = };")
-            vm.memory[1] = noun
-            vm.memory[2] = verb
+            vm.dbg = dbg.debug_level
+            vm.memory.set(1, noun)
+            vm.memory.set(2, verb)
             vm.core_dump_enabled = False
             vm.run()
-            if vm.memory[0] == target:
+            if vm.memory.get(0) == target:
                 return 100 * noun + verb
     return None
 
@@ -36,4 +38,5 @@ if __name__ == "__main__":
 
     print(problem(args.filename))
     print(problem(args.filename, part2=True))
+    print(f"{dbg.debug_level = }")
 
--- 2019/intcode.py
+++ 2019/intcode.py
@@ -1,15 +1,11 @@
 import enum
 from collections import defaultdict
 import itertools
+import sys
 
 if not hasattr(itertools, 'batched'):
     itertools.batched = lambda iterable, batch_size: zip(*(iterable[i::batch_size] for i in range(batch_size)))
 
-
-class Opcode(enum.Enum):
-    ADD = 1
-    MUL = 2
-    HALT = 99
 
 class IntcodeException(Exception):
     pass
@@ -17,64 +13,126 @@ class IntcodeException(Exception):
 class OutOfBoundsException(IntcodeException):
     pass
 
+class InvalidOpcode(IntcodeException):
+    pass
+
+
+class Instruction:
+    def __init__(self, memory, pc):
+        full_opcode = memory.get(pc)
+        self.op = Opcode.from_code(full_opcode % 100)
+        self.is_immediate = [full_opcode % (10**(x+3)) // (10**(x+2)) for x in range(self.op.potential_indirects)]
+        self.is_immediate.extend([True] * (self.op.param_count-self.op.potential_indirects))
+        self.parameters = [memory.get(pc+i) for i in range(1,self.op.param_count+1)]
+
+    def execute(self, machine):
+        parameters = [param if immediate else machine.memory.get(addr=param) for param,immediate in zip(self.parameters, self.is_immediate)]
+        self.op.function(machine, parameters)
+        machine.pc += self.op.param_count+1
+
+    def __str__(self):
+        result = [self.op.name]
+        result.append(', '.join(f'{"" if immediate else "*"}{param}' for param,immediate in zip(self.parameters, self.is_immediate)))
+        return " ".join(result)
+
+def nop(state):
+    pass
+
+
+class Opcode:
+    opcodes = {}
+    def __init__(self, name, code, param_count=0, potential_indirects=0, function=nop):
+        self.name = name
+        self.code = code
+        self.param_count = param_count
+        self.potential_indirects = potential_indirects
+        self.function = function
+        setattr(self.__class__, self.name, self)
+        self.__class__.opcodes[self.code] = self
+
+    def __repr__(self):
+        return f"<Opcode.{self.name}: {self.code}>"
+
+    @classmethod
+    def from_code(cls, code):
+        try:
+            return cls.opcodes[code]
+        except KeyError:
+            raise InvalidOpcode(f"Invalid opcode: {code}")
+
+Opcode("ADD", code=1, param_count=3, potential_indirects=2, function=lambda vm, params: vm.memory.set(params[2], params[0] + params[1]))
+Opcode("MUL", code=2, param_count=3, potential_indirects=2, function=lambda vm, params: vm.memory.set(params[2], params[0] * params[1]))
+Opcode("HALT", code=99, function=lambda vm, params: vm.halt())
+
+#class Opcode(enum.Enum):
+#    ADD   = 1
+#    MUL   = 2
+#    READ  = 3
+#    WRITE = 4
+#    HALT  = 99
+
+class Memory:
+    def __init__(self, data):
+        self.data = data
+
+    def set(self, addr, value):
+        try:
+            self.data[addr] = value
+        except IndexError:
+            raise OutOfBoundsException(f"Write error: {addr} is out of bounds")
+
+    def get(self, addr):
+        try:
+            return self.data[addr]
+        except IndexError:
+            raise OutOfBoundsException(f"Read error: {addr} is out of bounds")
+
+    def __str__(self):
+        return "\n".join(f"{','.join(str(item) for item in batch)}," for batch in itertools.batched(self.data, 4))
+
 
 class IntcodeVM:
-    def __init__(self, code):
+    def __init__(self, memory):
         self.tick_count = 0
         self.pc = 0
-        self.memory = code
-        self.halt = False
+        self.memory = memory
+        self.stdin = sys.stdin
+        self.stdout = sys.stdout
+        self.is_halted = False
         self.core_dump_enabled = True
-        self.dbg = False
+        self.dbg = 0
+
+    def halt(self):
+        self.is_halted = True
 
     def get_params(self, count):
         return self.memory[self.pc+1:self.pc+1+count]
 
-    def set(self, dst, value):
-        try:
-            self.memory[dst] = value
-        except IndexError:
-            raise OutOfBoundsException(f"Write error: {dst} is out of bounds")
-
-    def get(self, src):
-        try:
-            return self.memory[src]
-        except IndexError:
-            raise OutOfBoundsException(f"Read error: {src} is out of bounds")
-
     def tick(self):
         self.tick_count += 1
-        op = Opcode(self.memory[self.pc])
-        if op == Opcode.ADD:
-            a,b,dst = self.get_params(3)
-            self.set(dst, self.get(a) + self.get(b))
-            self.pc += 4
-        elif op == Opcode.MUL:
-            a,b,dst = self.get_params(3)
-            self.set(dst, self.get(a) * self.get(b))
-            self.pc += 4
-        elif op == Opcode.HALT:
-            self.halt = True
+        instruction = Instruction(self.memory, self.pc)
+        if self.dbg:
+            print(str(instruction))
+        instruction.execute(self)
 
     def run(self):
-        while not self.halt:
+        while not self.is_halted:
             try:
                 self.tick()
-                if self.dbg:
+                if self.dbg >= 2:
                     print(self)
             except IntcodeException as e:
                 print(f"Intcode program exception: {e}")
                 if self.core_dump_enabled:
                     print(self)
-                self.halt = True
+                self.halt()
         print("Program halted")
 
     def __str__(self):
         result = []
         result.append(f"tick {self.tick_count}")
-        result.append(f"{self.pc = }; {self.halt = };")
-        result.append(f"memory:")
-        result.extend(f"{','.join(str(item) for item in batch)}," for batch in itertools.batched(self.memory, 4))
+        result.append(f"{self.pc = }; {self.is_halted = };")
+        result.append(f"memory:\n{str(self.memory)}")
         result.append('\n')
         return '\n'.join(result)
 
@@ -84,4 +142,4 @@ class IntcodeVM:
         with open(file, 'r') as file:
             for line in file:
                 program.extend(int(token) for token in line.split(','))
-        return cls(program)
+        return cls(Memory(program))
