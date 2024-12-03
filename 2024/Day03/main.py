ae5ad6db35106adad2ef4e6031ebbdfedc11be0c
Alexander Martin <Alexander@theAlexanderMartin.com>
2024-12-03 00:20:26 -0500

    2024 Day 3

---
+++ 2024/Day03/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import argparse
+from aoc_util import dbg
+import re
+
+def problem(input_file, part2=False):
+    with open(input_file, 'r') as file:
+        lines = []
+        for line in file:
+            line = line.strip()
+            lines.append(line)
+    full_input = '\n'.join(lines)
+    result = re.findall("(mul\([\d]*,[\d]*\))|(do\(\))|(don't\(\))", full_input)
+    total = 0
+    enabled = True
+    for command in result:
+        # I'm doing my regex slightly wrong since it's returning a tuple. ¯\_(ツ)_/¯
+        command = ''.join(command)
+        if command.startswith("don't"):
+            enabled = False
+        elif command.startswith("do"):
+            enabled = True
+        elif (not part2 or enabled) and command.startswith("mul"):
+            command = command[4:-1]
+            x,y = command.split(',')
+            total += int(x) * int(y)
+    return total
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', nargs='?', default='input.txt')
+    parser.add_argument('-v', '--verbose', action='count', default=0)
+    args = parser.parse_args()
+    dbg.set_level(args.verbose)
+
+    print(problem(args.filename))
+    print(problem(args.filename, part2=True))
+
